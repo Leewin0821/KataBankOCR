a1032edc7fb9d4bfc2c7d2c14ebc39f90c45a8b6
Leewin0821 <leewin.zhang@gmail.com>
2015-02-24 00:54:46 +0800

    Finished User Story 1

--- src/test/java/DigitsParserTest.java
+++ src/test/java/DigitsParserTest.java
@@ -20,13 +20,26 @@ public class DigitsParserTest {
     }
 
     @Test
-    public void shouldReturnCorrectSingleDigitWhenGivenSingleDigitTextFile() throws Exception {
+    public void should_Return_Correct_Single_Digit_Given_Single_Digit_Text_File() throws Exception {
         //Given
         final String SINGLE_DIGIT_TEXT_FILE_PATH = "SingleDigitTextFile.txt";
-        int expectedResult = 4;
+        String expectedResult = "9";
 
         //When
-        int actualResult = digitsParser.parse(SINGLE_DIGIT_TEXT_FILE_PATH);
+        String actualResult = digitsParser.parse(SINGLE_DIGIT_TEXT_FILE_PATH);
+
+        //Then
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void should_Return_Correct_Digits_Given_Multiple_Digits_With_Same_Number_Text_File() throws Exception {
+        //Given
+        final String MULTIPLE_DIGITS_WITH_SAME_NUMBER_TEXT_FILE_PATH = "MultipleDigitsTextFileWIthSameNumber.txt";
+        String expectedResult = "888888888";
+
+        //When
+        String actualResult = digitsParser.parse(MULTIPLE_DIGITS_WITH_SAME_NUMBER_TEXT_FILE_PATH);
 
         //Then
         assertThat(actualResult, is(expectedResult));
