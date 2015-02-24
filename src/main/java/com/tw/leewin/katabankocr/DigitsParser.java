7e77602a75988517dcffe0750a5e48f1892936f8
lwzhang <lwzhang@thoughtworks.com>
2015-02-24 14:04:16 +0800

    Refactor DigitsParser class

--- src/main/java/com/tw/leewin/katabankocr/DigitsParser.java
+++ src/main/java/com/tw/leewin/katabankocr/DigitsParser.java
@@ -1,24 +1,22 @@
 package com.tw.leewin.katabankocr;
 
-import java.io.*;
+import java.io.IOException;
+import java.util.List;
+
+import static com.tw.leewin.katabankocr.SymbolArray.ACCOUNT_COLUMN_NUMBER;
+import static com.tw.leewin.katabankocr.SymbolArray.ACCOUNT_ROW_NUMBER;
 
 /**
  * Created by leewin on 15/2/19.
  */
 public class DigitsParser {
 
-    private static final int ACCOUNT_ROW_NUMBER = 3;
-    private static final int ACCOUNT_COLUMN_NUMBER = 27;
-
-    private char[][] symbolArray = new char[ACCOUNT_ROW_NUMBER][ACCOUNT_COLUMN_NUMBER];
+    private SymbolArray symbolArray = new SymbolArray();
     private SymbolToNumberMapper mapper = new SymbolToNumberMapper();
 
-    public DigitsParser() {
-    }
-
     public String parse(String filePath) throws IOException {
-        initSymbolArray();
-        initFile(filePath);
+        List<String> symbolsList = DigitsFileReader.getInstance().readDigitFile(filePath);
+        symbolArray.saveSymbols(symbolsList);
         return parseSymbols();
     }
 
@@ -27,48 +25,13 @@ public class DigitsParser {
         for (int columnIndex = 0; columnIndex < ACCOUNT_COLUMN_NUMBER; columnIndex = columnIndex + 3) {
             String symbol = "";
             for (int rowIndex = 0; rowIndex < ACCOUNT_ROW_NUMBER; rowIndex++) {
-                symbol += "" + symbolArray[rowIndex][columnIndex] +
-                        symbolArray[rowIndex][columnIndex + 1] +
-                        symbolArray[rowIndex][columnIndex + 2];
+                symbol += "" + symbolArray.getSymbol(rowIndex, columnIndex) +
+                        symbolArray.getSymbol(rowIndex, columnIndex + 1) +
+                        symbolArray.getSymbol(rowIndex, columnIndex + 2);
             }
-            for (int index = 0; index < Numbers.values().length; index++) {
-//                if (mapper.containsSymbol(symbol)){
-//                    symbols += mapper.getNumber(symbol);
-//                } else {
-//                    symbols += "? ILL";
-//                }
-                if (symbol.equals(Numbers.values()[index].getSymbols())) {
-                    symbols += Numbers.values()[index].getNumber();
-                }
-            }
+            symbols += mapper.getNumber(symbol);
         }
         return symbols;
     }
 
-    private void initFile(String filePath) throws IOException {
-        File file = new File(filePath);
-        int rowIndex = -1;
-        String inputLine = "";
-        if (file.isFile() && file.exists()) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            while ((inputLine = bufferedReader.readLine()) != null) {
-                rowIndex++;
-                System.out.println(inputLine);
-                for (int columnIndex = 0; columnIndex < inputLine.length(); columnIndex++) {
-                    symbolArray[rowIndex][columnIndex] = inputLine.charAt(columnIndex);
-                }
-            }
-            bufferedReader.close();
-        } else {
-            System.out.println("No such file!");
-        }
-    }
-
-    private void initSymbolArray() {
-        for (int rowIndex = 0; rowIndex < ACCOUNT_ROW_NUMBER; rowIndex++) {
-            for (int columnIndex = 0; columnIndex < ACCOUNT_COLUMN_NUMBER; columnIndex++) {
-                symbolArray[rowIndex][columnIndex] = ' ';
-            }
-        }
-    }
 }
--- src/test/java/DigitsParserTest.java
+++ src/test/java/DigitsParserTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class DigitsParserTest {
     private DigitsParser digitsParser;
@@ -19,36 +19,11 @@ public class DigitsParserTest {
         digitsParser = null;
     }
 
-    @Test
-    public void should_return_correct_single_digit_given_single_digit() throws Exception {
-        //Given
-        final String SINGLE_DIGIT_TEXT_FILE_PATH = "SingleDigitTextFile.txt";
-        String expectedResult = "9";
-
-        //When
-        String actualResult = digitsParser.parse(SINGLE_DIGIT_TEXT_FILE_PATH);
-
-        //Then
-        assertThat(actualResult, is(expectedResult));
-    }
-
-    @Test
-    public void should_Return_Correct_Digits_Given_Multiple_Digits_With_Same_Number_Text_File() throws Exception {
-        //Given
-        final String MULTIPLE_DIGITS_WITH_SAME_NUMBER_TEXT_FILE_PATH = "MultipleDigitsTextFileWIthSameNumber.txt";
-        String expectedResult = "888888888";
-
-        //When
-        String actualResult = digitsParser.parse(MULTIPLE_DIGITS_WITH_SAME_NUMBER_TEXT_FILE_PATH);
-
-        //Then
-        assertThat(actualResult, is(expectedResult));
-    }
 
     @Test
     public void should_return_correct_digits_given_multiple_digits_with_same_number() throws Exception {
         //Given
-        final String MULTIPLE_DIGITS_WITH_SAME_NUMBER_TEXT_FILE_PATH = "MultipleDigitsTextFileWithSameNumber.txt";
+        String MULTIPLE_DIGITS_WITH_SAME_NUMBER_TEXT_FILE_PATH = getClass().getClassLoader().getResource("MultipleDigitsTextFileWIthSameNumber.txt").getPath();
         String expectedResult = "888888888";
 
         //When
@@ -61,7 +36,7 @@ public class DigitsParserTest {
     @Test
     public void should_return_correct_digits_given_multiple_digits_with_different_number() throws Exception {
         //Given
-        final String MULTIPLE_DIGITS_WITH_DIFFERENT_NUMBER_TEXT_FILE_PATH = "MultipleDigitsTextFileWIthDifferentNumber.txt";
+        String MULTIPLE_DIGITS_WITH_DIFFERENT_NUMBER_TEXT_FILE_PATH = getClass().getClassLoader().getResource("MultipleDigitsTextFileWIthDifferentNumber.txt").getPath();
         String expectedResult = "123456789";
 
         //When
@@ -70,17 +45,4 @@ public class DigitsParserTest {
         //Then
         assertThat(actualResult, is(expectedResult));
     }
-
-//    @Test
-//    public void should_return_digits_with_error_symbol_given_multiple_digits_with_error_number() throws Exception {
-//        //Given
-//        final String MULTIPLE_DIGITS_WITH_ERROR_NUMBER_TEXT_FILE_PATH = "MultipleDigitsTextFileWithErrorNumber.txt";
-//        String expectedResult = "12345678? ILL";
-//
-//        //When
-//        String actualResult = digitsParser.parse(MULTIPLE_DIGITS_WITH_ERROR_NUMBER_TEXT_FILE_PATH);
-//
-//        //Then
-//        assertThat(actualResult, is(expectedResult));
-//    }
 }
